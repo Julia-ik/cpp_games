dfb1f20435f1d77e6005d35d14193bcc6f89c355
Julia-ik <lilu.just@mail.ru>
2021-11-12 12:59:23 +0300

    hw1

---
+++ hw_1/untitled/main.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <stdlib.h>
+
+void print(int n) {
+    if (n < 0)
+    {
+        putchar('-');
+        n = -n;
+    }
+    if (n / 10 != 0)
+    {
+        print(n / 10);
+    }
+
+    putchar((n % 10) + '0');
+}
+int main()
+{
+    int k;
+    bool is_success= false;
+    while(!is_success)
+    {
+        printf("ent num(0-90)");
+        scanf("%d", &k);
+
+        if (k > 90 || k < 0)
+        {
+            printf("error\n");
+        }
+        else {
+            print(rand() % k);
+            printf("\n");
+            fflush(stdout);
+            is_success=true;
+        }
+    }
+
+    return 0;
+}
